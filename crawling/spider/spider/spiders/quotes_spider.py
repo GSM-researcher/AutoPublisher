05ce4bc0ebfdc2f0bce1de20b7973fe179d59499
Ldoun <67096173+Ldoun@users.noreply.github.com>
2021-03-15 20:00:31 +0900

    ONLY html
    google

--- crawling/spider/spider/spiders/quotes_spider.py
+++ crawling/spider/spider/spiders/quotes_spider.py
@@ -10,24 +10,35 @@ class SpiderSpider(CrawlSpider):
                 super().__init__(*a, **kw)
 
         def parse(self, response):
+                if "google" in response.url:
+                        return
+                print(response.url)
                 filename = str(self.count) + '.html'
-                self.count +=1
-                with open('../downloded_html/'+filename, 'wb') as f:
-                        f.write(response.body)
-                        soup = BeautifulSoup(response.body,'html.parser')
-                        for i in soup.select('link'):
-                                if "stylesheet" in i['rel']:
-                                        domain = response.url.split('.com')[0]+'.com'
-                                        print(domain)
-                                        css = i['href'].split('.css')[0]+'.css'
-                                        css_address = domain + css
-                                        print(css_address)
-                                        page = requests.get(css_address,verify=False)
-                                        with open('../downloded_html/css/' + self.count + '.css','w') as w:
-                                                w.write(page)                    
+                flag = 1
+                soup = BeautifulSoup(response.body,'html.parser')
+                for i in soup.select('link'):
+                        if "stylesheet" in i['rel']:
+                                flag = 0
+                                domain = response.url.split('.com')[0]+'.com'
+                                css = i['href'].split('.css')[0]+'.css'
+                                
+                                '''css_address = domain + css
+                                print(css_address)
+                                page = requests.get(css_address,verify=False)
+                                with open('../downloded_html/css/' + self.count + '.css','w') as w:
+                                        w.write(page)'''   
+                if flag == 1:
+                        with open('../downloded_html/'+filename, 'wb') as f:
+                                print()
+                                print(filename)
+                                print()
+                                
+                                f.write(response.body)
+                                self.count +=1
+                                        
         
         name = 'spider'
-        start_urls = ['https://www.google.com/search?q=%EC%9D%B8%EA%B3%B5%EC%A7%80%EB%8A%A5&oq=%EC%9D%B8%EA%B3%B5%EC%A7%80%EB%8A%A5&aqs=chrome..69i57j35i39j69i60j69i65l3j69i61l2.3685j0j4&sourceid=chrome&ie=UTF-8']
+        start_urls = ['https://www.google.com/search?q=%ED%81%AC%EB%A1%A4%EB%A7%81&oq=%ED%81%AC%EB%A1%A4%EB%A7%81&aqs=chrome..69i57j35i39j0i131i433j0i433j0i131i433j0i433j0j69i61.2498j1j4&sourceid=chrome&ie=UTF-8']
 
         rules = [Rule(LinkExtractor(),
-                      callback='parse',follow=True)]
+                      callback='parse',follow=True,)]
